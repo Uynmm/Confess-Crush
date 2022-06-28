69d695fe334c5ccfdcc0a2fe8023b0aa61b7a289
Uynmm <108325105+Uynmm@users.noreply.github.com>
2022-06-28 21:07:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "thế giới của anh <333 ",
+    introTitle: 'đồ ngốc đáng iuuu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -13,8 +13,8 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Anh biết mà 🥰. Yêu bé nhiều nhiều 😘😘',
+    messDesc: ' Anh chỉ muốn nói là, quá khữ có thể mình không quen biết nhau, nhưng hiện tại thì mình đã và đang đi cùng nhau trên một đoạn đường dài. Nói là dài thì cũng không đúng, nhưng hè năm lớp 11 này- kì nghỉ hè cuối cùng của đời học sinh này đây anh cảm thấy nó thật đẹp và ý nghĩa khi có em. Căm ơn em đã đến. Có lẽ mn nói đúng, tình năm 17 tuổi sẽ luôn thật đẹp, khi mà cả hai thật lòng. Đây kh phải lần đầu tên anh yêu một người, nhưng lần đầu tiên t khóc vì một người, trong một khoảnh khắc nào đấy, em đã mang đến cho t một cảm giác  khó diễn tả mà không ai có thể làm được. Điều quan trọng phải nói nhiều lần, anh thẬt sự yêu bé, hi vọng là mình cũng có thể đi cùng nhau trên đoạn đường mang tên  "những ngày tháng của sau này".  ',
+    btnAccept: 'Yêu LAn NHất trên đờiii <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
